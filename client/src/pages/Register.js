53fa90c98b6a41a3f170a530bbd256491c74b685
Sreedhar Vangala <sridharvangala741@gmail.com>
2022-03-04 08:10:02 +0530

    Changed commit

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -18,18 +18,23 @@ function Register() {
 
   return (
     <div className="login">
-      {loading && (<Spinner />)}
+      {loading && <Spinner />}
       <Row gutter={16} className="d-flex align-items-center">
         <Col lg={16} style={{ position: "relative" }}>
-          <img 
-           className='w-100'
-           data-aos='slide-left'
-           data-aos-duration='1500'
-          src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
+          <img
+            className="w-100"
+            data-aos="slide-left"
+            data-aos-duration="1500"
+            src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+          />
           <h1 className="login-logo">SREECARS</h1>
         </Col>
         <Col lg={8} className="text-left p-5">
-          <Form layout="vertical" className="login-form p-5" onFinish={onFinish}>
+          <Form
+            layout="vertical"
+            className="login-form p-5"
+            onFinish={onFinish}
+          >
             <h1>Register</h1>
             <hr />
             <Form.Item
@@ -44,14 +49,14 @@ function Register() {
               label="Password"
               rules={[{ required: true }]}
             >
-              <Input />
+              <Input type="password" />
             </Form.Item>
             <Form.Item
               name="cpassword"
               label="Confirm Password"
               rules={[{ required: true }]}
             >
-              <Input />
+              <Input type="password" />
             </Form.Item>
 
             <button className="btn1 mt-2 mb-3">Register</button>
